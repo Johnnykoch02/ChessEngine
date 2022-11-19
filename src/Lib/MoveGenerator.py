7c2435a4f37884f8fd3ab85107ab591e85e4c70f
JohnKoch02 <johnnykoch02@gmail.com>
2022-11-19 15:19:49 -0500

    (Added): Move Generation Logic, Virtual Representation of Board, Virtual Representation of Pieces

--- src/Lib/MoveGenerator.py
+++ src/Lib/MoveGenerator.py
@@ -22,21 +22,81 @@ class MoveGenerator:
         initial_pos = piece.square
         for move_type in MoveTypes:
             if move_type == NSEW:
-                n, s, e, w = (0, -1), (0,1), (1,0), (-1,0)
-                MoveGenerator.MoveInDirection(PseudoLegalMoves, piece, add_two_pos(initial_pos, n), board, n) # North
-                MoveGenerator.MoveInDirection(PseudoLegalMoves, piece, add_two_pos(initial_pos, s), board, s) #South
-                MoveGenerator.MoveInDirection(PseudoLegalMoves, piece, add_two_pos(initial_pos, e), board, e) #East
-                MoveGenerator.MoveInDirection(PseudoLegalMoves, piece, add_two_pos(initial_pos, w), board, w) #West
-            
+                n, s, e, w = (-1, 0), (1,0), (0,1), (0,-1)
+                MoveGenerator.MoveInDirectionNorm(PseudoLegalMoves, piece, add_two_pos(initial_pos, n), board, n) # North
+                MoveGenerator.MoveInDirectionNorm(PseudoLegalMoves, piece, add_two_pos(initial_pos, s), board, s) # South
+                MoveGenerator.MoveInDirectionNorm(PseudoLegalMoves, piece, add_two_pos(initial_pos, e), board, e) # East
+                MoveGenerator.MoveInDirectionNorm(PseudoLegalMoves, piece, add_two_pos(initial_pos, w), board, w) # West
+            if move_type == DIAGANOL:
+                ne, se, nw, sw = (1, -1), (1, 1), (-1,-1), (1,-1)
+                MoveGenerator.MoveInDirectionNorm(PseudoLegalMoves, piece, add_two_pos(initial_pos, n), board, ne) # NE
+                MoveGenerator.MoveInDirectionNorm(PseudoLegalMoves, piece, add_two_pos(initial_pos, s), board, se) #SE
+                MoveGenerator.MoveInDirectionNorm(PseudoLegalMoves, piece, add_two_pos(initial_pos, e), board, nw) #NW
+                MoveGenerator.MoveInDirectionNorm(PseudoLegalMoves, piece, add_two_pos(initial_pos, w), board, sw) #SW
+            if move_type == PAWN_WHITE:
+                MoveGenerator.pawn_moves(PseudoLegalMoves, piece, initial_pos, board, 1)
+            if move_type == PAWN_BLACK:
+                MoveGenerator.pawn_moves(PseudoLegalMoves, piece, initial_pos, board, -1)
+            if move_type == KING:
+                MoveGenerator.king_moves(PseudoLegalMoves, piece, initial_pos, board)
 
     @staticmethod
-    def MoveInDirection(PsuedoLegalMoves, piece, pos, board, direction):
+    def king_moves(PseudoLegalMoves, piece, pos, board):
+        poses = add_two_pos(pos, (-1, -1)), add_two_pos(pos, (-1,0)), add_two_pos(pos, (-1,1)), add_two_pos(pos, (0, 1)), add_two_pos(pos, (0,-1)), add_two_pos(pos, (1,0)), add_two_pos(pos, (1,-1)), add_two_pos(pos, (1,1)) 
+        valid_poses = []
+        for spot in poses:
+            if is_in_board(spot):
+                valid_poses.append(spot)
+        '''
+            - Finish the King Logic
+            - Finish Horsie Logic
+            DONE FOR PSEUDOLEGAL
+            - take the pseudolegal moves, play the move on a virtual board, check to see if the king is in check
+            - if the king is not in check, then the move is valid
+
+            Move Generation:
+            - Check if the king is in check
+                -if the king is in check, then you need to only be able to play those moves.
+                -if the king is not in check, then generate all the pseudolegal moves.
+                - Play all of the pseudolegal moves that we just made, and get the legal moves.
+
+        '''
+
+    @staticmethod
+    def pawn_moves(PsuedoLegalMoves, piece, pos, board, direction):
+        if direction == 1 and pos[0] == 1: #White Piece
+            #Beginning Piece
+            temp_piece = board.get_square(add_two_pos(pos, (2,0)))
+            if temp_piece is None:
+                PsuedoLegalMoves.add(add_two_pos(pos, (2,0)))
+        
+        if direction == -1 and pos[0] == 6: # Black Piece
+            #Beginning Piece
+            temp_piece = board.get_square(add_two_pos(pos, (-2,0)))
+            if temp_piece is None:
+                PsuedoLegalMoves.add(add_two_pos(pos, (-2,0)))
+
+        # Straight Forward  
+        temp_piece = board.get_square(add_two_pos(pos, (direction,0)))
+        if temp_piece is None:
+            PsuedoLegalMoves.add(add_two_pos(pos, (direction, 0)))
+        # 1 Diagonal    
+        temp_piece = board.get_square(add_two_pos(pos, (direction, direction)))
+        if temp_piece is not None:
+            PsuedoLegalMoves.add(add_two_pos(pos, (direction, direction)))
+        # 1 Diagonal
+        temp_piece = board.get_square(add_two_pos(pos, (direction, -direction)))
+        if temp_piece is not None:
+            PsuedoLegalMoves.add(add_two_pos(pos, (direction, -direction)))                   
+                
+    @staticmethod
+    def MoveInDirectionNorm(PsuedoLegalMoves, piece, pos, board, direction):
         if not is_in_board(pos):
             return
         temp_piece = board.get_square(pos)
         if temp_piece is None:
             PsuedoLegalMoves.add(pos)
-            MoveGenerator.MoveInDirection(PsuedoLegalMoves, piece, add_two_pos(pos, direction), board, direction)
+            MoveGenerator.MoveInDirectionNorm(PsuedoLegalMoves, piece, add_two_pos(pos, direction), board, direction)
         else:
             if temp_piece.color != piece.color:
                 PsuedoLegalMoves.add(pos)
--- src/Managers/AppManager.py
+++ src/Managers/AppManager.py
@@ -59,6 +59,8 @@ class AppManager:
         self.current_player.next = Player('white')
         self.current_player.next.next = self.current_player
         self.piece_selected = None
+
+        self.board.current_color = self.current_player.type
         # Start the App
 
         self.Run()
@@ -89,6 +91,7 @@ class AppManager:
 
     def mouse_events(self, mouse_pos, presses): 
         square_on = (mouse_pos[1]//SQUARE_DIMENSIONS[0], mouse_pos[0]//SQUARE_DIMENSIONS[0]) # Mouse to Square
+        print(square_on)
         if presses[0]:
            if self.piece_selected:
             pq = self.board.get_square(square_on) # Query the board
@@ -96,11 +99,14 @@ class AppManager:
                 if self.piece_selected != pq and pq:
                     self.board.remove_piece(pq)
                     pq.destroy()
+                made_move_on_board = not (self.piece_selected.square == square_on)
                 self.piece_selected.square = square_on
                 self.piece_selected.selected = False
                 self.board.place_piece(self.piece_selected)
                 self.piece_selected = None
-                self.current_player = self.current_player.next                    
+                if made_move_on_board:
+                    self.current_player = self.current_player.next  
+                    self.board.current_color = self.current_player.type                  
            else: #We need to validate the proper color has been selected
                 temp_piece = self.board.get_square(square_on)
                 if temp_piece is not None and temp_piece.color == self.current_player.type:
